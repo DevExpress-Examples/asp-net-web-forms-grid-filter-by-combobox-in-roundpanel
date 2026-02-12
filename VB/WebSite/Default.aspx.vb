aaec0ae529ceb7586a284e8b6b041cd62081d8a9
Sarmin Anton <anton.sarmin@devexpress.com>
2026-02-12 12:54:59 +0400

    fix vb

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,18 @@
+﻿Option Infer On
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub ASPxComboBoxCategoriesInHeader_Init(ByVal sender As Object, ByVal e As EventArgs)
+		Dim cb = DirectCast(sender, ASPxComboBox)
+		Dim controlParameter As ControlParameter = CType(SqlDataSourceProducts3.SelectParameters(0), ControlParameter)
+		controlParameter.ControlID = cb.UniqueID
+	End Sub
+End Class
